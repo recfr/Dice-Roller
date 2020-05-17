9ee50cf3185fffb0172b820d3fd5ab56f1825840
Recep FIRINCIOĞLU <recep.firincioglu@gmail.com>
2020-05-17 10:32:00 +0300

    lateinit var implemented for increment efficiency

--- app/src/main/java/com/recepfr/diceroller/MainActivity.kt
+++ app/src/main/java/com/recepfr/diceroller/MainActivity.kt
@@ -6,21 +6,23 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
 
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
 //        Toast.makeText(this, "Button clicked.", Toast.LENGTH_SHORT).show()
-
         val randomInt = (1..6).random()
-
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -30,9 +32,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
-
     }
-
 }
