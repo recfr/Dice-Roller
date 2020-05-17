105d7e1a3841375ad15965fe67e263884e4848a5
Recep FIRINCIOĞLU <recep.firincioglu@gmail.com>
2020-05-17 11:58:23 +0300

    Double dice implemented.

--- app/src/main/java/com/recepfr/diceroller/MainActivity.kt
+++ app/src/main/java/com/recepfr/diceroller/MainActivity.kt
@@ -7,23 +7,29 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
-
+        rollButton.setOnClickListener { rollDice() }
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+
     }
+
+    /*** ----------------------------------------------------------------------------------------- ***/
 
     private fun rollDice() {
 //        Toast.makeText(this, "Button clicked.", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val drawableResource = when (randomInt) {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int {
+        return when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,7 +37,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
     }
 }
